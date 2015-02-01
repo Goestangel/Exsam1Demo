ecf522a8b71a787676bd0b1c9a68dd6b2eab3e4f
Goestangel <petqgosheva@yahoo.com>
2015-02-02 00:10:15 +0200

    .init
    Demos

---
+++ 01Problem 1 3 6 9/SecretCode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+static void Main()
+  {
+    int A = int.Parse(Console.ReadLine());
+    int B = int.Parse(Console.ReadLine());
+    int C = int.Parse(Console.ReadLine());
+    int R = 0;
+
+    switch (B)
+    {   
+        case 3: R = A + C; break;
+        case 6: R = A * C; break;
+        case 9: R = A % C; break;            
+    }
+    int Result = R % 3 == 0 ? R / 3 : R % 3;
+    Console.WriteLine(Result);
+    Console.WriteLine(R);
+  }
+}
+
---
+++ 02The Horror/Horror.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Horror
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        
+        int position = 0;
+        int counter = 0;
+        ulong result = 0;
+
+
+        foreach (char symbol in input)
+        {
+            int digit = symbol - '0';
+            position++;
+
+            if (position % 2 != 0)
+            {
+                result = result + (ulong)digit;
+                counter++;
+
+            }
+
+        }
+        Console.WriteLine(counter +" "+ result);
+
+
+
+    }
+}
+
